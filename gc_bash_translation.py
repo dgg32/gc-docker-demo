b3a0e0373bd8dadb142e8b5e9caf610e3a52787a
dgg32 <dgg321982@gmail.com>
2022-04-09 22:26:41 +0800

    add test

--- gc_bash_translation.py
+++ gc_bash_translation.py
@@ -9,15 +9,20 @@
 # echo $(((cCount + gCount) * 100/(cCount + gCount + aCount + tCount + nCount)))
 
 #!/usr/bin/env python 
-import sys
-input_file = sys.argv[1]
 
-sequence = "".join([x for x in open(input_file).read().splitlines() if not x.startswith(">")])
+def calculate_gc(sequence):
+    aCount = sequence.count('A')
+    gCount = sequence.count('G')
+    cCount = sequence.count('C')
+    tCount = sequence.count('T')
+    nCount = sequence.count('N')
 
-aCount = sequence.count('A')
-gCount = sequence.count('G')
-cCount = sequence.count('C')
-tCount = sequence.count('T')
-nCount = sequence.count('N')
+    return int((cCount + gCount) * 100/(aCount + gCount + cCount + tCount + nCount))
 
-print (int((cCount + gCount) * 100/(aCount + gCount + cCount + tCount + nCount)))
+if __name__ == '__main__':
+    import sys
+    input_file = sys.argv[1]
+
+    sequence = "".join([x for x in open(input_file).read().splitlines() if not x.startswith(">")])
+
+    print (calculate_gc(sequence))
